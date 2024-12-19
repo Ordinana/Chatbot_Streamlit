a7dc2845f494c17a56ee129f19178ba671ce23bf
Carlos Ordiñana Caballer <carlosoa34@gmail.com>
2024-12-19 13:27:05 +0100

    mejora app

--- app.py
+++ app.py
@@ -1,115 +1,100 @@
-# Comprobar versiones de librerias para requirements.txt usar "pip freeze" en terminar con env activado
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import os
-from IPython.display import Markdown
+debug = True
 
 # Librerías para la preparación de datos
-from langchain.document_loaders import PyPDFDirectoryLoader  
-from langchain.text_splitter import RecursiveCharacterTextSplitter  
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
-from langchain.embeddings import SentenceTransformerEmbeddings
 
 # Librerías para el proceso de Retrieval
-from langchain import hub  
-from langchain_core.output_parsers import StrOutputParser 
-from langchain_core.runnables import RunnablePassthrough 
-from langchain_google_genai import ChatGoogleGenerativeAI 
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.schema import Document
+
+from dotenv import load_dotenv
+load_dotenv()
 
 import streamlit as st
-from io import StringIO
 
-import cohere
-import numpy as np
-from dotenv import load_dotenv
-
-from langchain.vectorstores import Chroma
-from langchain.embeddings import CohereEmbeddings
-
-import PyPDF2
-
-# Configuración de la página
 st.set_page_config(
-    page_title="Hazle preguntas a Gemini!",
+    page_title="RAG LANGCHAIN GEMINI App",
     page_icon=":rocket:",
     layout="wide",
+    initial_sidebar_state="expanded",
+
 )
 
-# Encabezados de la interfaz
-st.markdown("<h1 style='text-align: center;'>Chatbot de Gemini 🤖</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center;'>Sube un archivo PDF, escribe una pregunta y obtén respuestas inteligentes.</h2>", unsafe_allow_html=True)
+st.title("RAG LANGCHAIN GEMINI App")
 
-# Cargar variables de entorno
-load_dotenv()
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-
-# Procesamiento del archivo PDF
-file = st.file_uploader("Carga un fichero PDF", type="pdf") # Para la subida del pdf
-source_data_folder = "./ficheros"
-
+file = st.file_uploader("Upload a PDF file", accept_multiple_files=False, type="pdf")
+source_data_folder = "./content/MisDatos"
 if file:
-    with open(source_data_folder + "/PDF.pdf", "wb") as f:
+    with open(source_data_folder+"/pdf.pdf", 'wb') as f: 
+        # f.write(file)
         f.write(file.getvalue())
 
-# Leer el archivo PDF 
+print("librerias cargadas")
+# source_data_folder = "./content/MisDatos"
+# Leyendo los PDFs del directorio configurado
 loader = PyPDFDirectoryLoader(source_data_folder)
-data_on_pdf = loader.load() 
+data_on_pdf = loader.load()
+# cantidad de data cargada
+if debug:
+    print("pdf cargado")
 
-# División del texto. Con un tamaño delimitado (chunks) y 
+# Particionando los datos. Con un tamaño delimitado (chunks) y 
 # 200 caracters de overlapping para preservar el contexto
-text_splitter = RecursiveCharacterTextSplitter( 
-    separators=["\n\n", "\n", ". ", " "],
+text_splitter = RecursiveCharacterTextSplitter(
+    separators=["\n\n", "\n", ". ", " ", ""],
     chunk_size=1000,
     chunk_overlap=200
 )
 splits = text_splitter.split_documents(data_on_pdf)
-print("-------------------------", splits)
+# Cantidad de chunks obtenidos
+print("[INFO] splits preparados")
+
+from langchain.vectorstores import Chroma
+from langchain.embeddings import CohereEmbeddings
 
 # Crea la instancia de embeddings con Cohere
-# embeddings_model = CohereEmbeddings(cohere_api_key=os.environ["COHERE_API_KEY"], user_agent="Ordi")  
-embeddings_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=os.environ["GOOGLE_API_KEY"])
+# embeddings_model = CohereEmbeddings(cohere_api_key=os.environ["COHERE_API_KEY"], user_agent="Fran")
+embeddings_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=os.environ["JUAN"])
 path_db = "./content/VectorDB"  # Ruta a la base de datos del vector store
 
 # Crear el vector store a partir de tus documentos 'splits'
-vectorstore = Chroma.from_documents(   
+vectorstore = Chroma.from_documents(
     documents=splits, 
     embedding=embeddings_model, 
-    persist_directory=path_db # persist = guardarlos: Para que podamos persistir los datos y no generarlos todo el tiempo
+    persist_directory=path_db
 )
 
-# Definición del modelo LLM
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=os.environ["GOOGLE_API_KEY"])
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", 
+                             google_api_key=os.environ["JUAN"])
 
-# Utiliza el vector de Chroma
+
 retriever = vectorstore.as_retriever()
-
-# Definición del prompt
 prompt = hub.pull("rlm/rag-prompt")
 
 def format_docs(docs):
     # Funcion auxiliar para enviar el contexto al modelo como parte del prompt
     return "\n\n".join(doc.page_content for doc in docs)
 
-# Define un flujo (chain) 
 rag_chain = (
     {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt # Es una plantilla donde incluye la  pregunta para el modelo
-    | llm # El modelo
-    | StrOutputParser() # La forma en la que te devuelve la respuesta del modelo
+    | prompt
+    | llm
+    | StrOutputParser()
 )
 
-prompt.messages[0].prompt.template = "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. Answer in spanish. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.\nQuestion: {question} \nContext: {context} \nAnswer:"
+pregunta = st.text_input("ASK: ", "Que es la pacobilla?")
 
-# Entrada del usuario y consulta
-user_input = st.text_input("Cuál es tu pregunta:")
-
-if st.button("PREGUNTAR", type="primary"):
-    if user_input:
-        response = rag_chain.invoke(user_input)
-        Markdown(response)
-
-        # st.write("Pregunta: " + user_input)
-        st.header("Respuesta generada: " )
-        st.write(response)
+if st.button("RUN QUERY"):
+    response = rag_chain.invoke(pregunta)
+    print(response)
+    st.write(response)
